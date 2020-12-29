6c4870f09200af5f9c47af97285d0221d9b0d201
vineetwani <61347018+vineetwani@users.noreply.github.com>
2020-12-29 11:16:56 +0530

    New Thread created

--- Practice.py
+++ Practice.py
@@ -1,4 +1,13 @@
 import threading as th
 
-def startFunc():
-  print("THis is my THread")
+
+def start_func():
+    for i in range(1000):
+        print("This is my Thread")
+
+
+if __name__ == "__main__":
+    th1 = th.Thread(target=start_func)
+    th1.start()
+    th1.join()
+    print("Main Ends")
